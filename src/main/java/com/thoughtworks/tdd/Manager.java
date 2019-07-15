57fba9c50ead90b19df1d8c91ffe16291041666c
zzw <kenzzw@163.com>
2019-07-15 23:38:46 +0800

    Refactor: refactor class Manager

--- src/main/java/com/thoughtworks/tdd/Manager.java
+++ src/main/java/com/thoughtworks/tdd/Manager.java
@@ -5,35 +5,19 @@ import java.util.List;
 
 public class Manager extends ParkingBoy{
     private List<ParkingBoy> parkingBoys;
-
     public Manager(ParkingLot parkingLot) {
         super(parkingLot);
         this.parkingBoys=new ArrayList<>();
     }
-
-    public List<ParkingBoy> getParkingBoys() {
-        return parkingBoys;
-    }
-
-    public void setParkingBoys(List<ParkingBoy> parkingBoys) {
-        this.parkingBoys = parkingBoys;
-    }
-
-    public Manager(List<ParkingLot> parkingLots) {
-        super(parkingLots);
-    }
-
     public void manage(ParkingBoy parkingBoy) throws ParkingException {
         if(parkingBoy==null){
             throw new ParkingException("parking boy is null");
         }
         this.parkingBoys.add(parkingBoy);
     }
-
     public Ticket managePark(ParkingBoy parkingBoy,Car car) throws ParkingException {
        return  parkingBoy.parking(car);
     }
-
     public Car manageParkFetch(ParkingBoy parkingBoy,Ticket ticket) throws Exception {
        return  parkingBoy.fetchCar(ticket);
     }
