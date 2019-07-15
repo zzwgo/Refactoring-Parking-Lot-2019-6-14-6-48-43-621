ee7b8ab9089a4eeee37c46a635983a7e4a222751
zzw <kenzzw@163.com>
2019-07-15 23:26:27 +0800

    Refactor: refactor SmartPakingBoy class

--- src/main/java/com/thoughtworks/tdd/SmartPakingBoy.java
+++ src/main/java/com/thoughtworks/tdd/SmartPakingBoy.java
@@ -4,37 +4,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SmartPakingBoy extends ParkingBoy {
-
     public SmartPakingBoy(ParkingLot parkingLot) {
         super(parkingLot);
     }
-
     public SmartPakingBoy(List<ParkingLot> parkingLots) {
         super(parkingLots);
     }
 
+    private int chooseLot(){
+        int loopIndex=0;
+        int chooseLotIndex=0;
+        for(ParkingLot parkingLot:parkingLots){
+            if(!parkingLots.get(chooseLotIndex).isFull() && !parkingLot.isFull()){
+                chooseLotIndex=parkingLots.get(chooseLotIndex).getParkingMap().size()<=parkingLot.getParkingMap().size()?chooseLotIndex:loopIndex;
+            }
+            if(parkingLots.get(chooseLotIndex).isFull()&&chooseLotIndex<parkingLots.size()){
+                chooseLotIndex++;
+            }
+            loopIndex++;
+        }
+        return chooseLotIndex;
+    }
     @Override
     public Ticket parking(Car car) throws ParkingException {
-        int index=0;
-        int chooseLot=0;
-
         for(ParkingLot parkingLot:parkingLots){
             if(parkingLot.hasBeenPark(car)){
                 return null;
             }
-            if(!parkingLots.get(chooseLot).isFull() && !parkingLot.isFull()){
-                    chooseLot=parkingLots.get(chooseLot).getParkingMap().size()<=parkingLot.getParkingMap().size()?chooseLot:index;
-            }
-            if(parkingLots.get(chooseLot).isFull()&&chooseLot<parkingLots.size()){
-                chooseLot++;
-            }
-            index++;
         }
-        if(parkingLots.get(chooseLot).isFull()){
+        if(parkingLots.get(chooseLot()).isFull()){
             throw new ParkingException("Not enough position.");
         }else{
             Ticket ticket=new Ticket();
-            parkingLots.get(chooseLot).park(ticket,car);
+            parkingLots.get(chooseLot()).park(ticket,car);
             return ticket;
         }
     }
