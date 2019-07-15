aaa2593b31dc17a9af90231ac5a104c71344bc8c
zzw <kenzzw@163.com>
2019-07-15 22:57:01 +0800

    Refactor: refactor class ParkingBoy

--- src/main/java/com/thoughtworks/tdd/ParkingBoy.java
+++ src/main/java/com/thoughtworks/tdd/ParkingBoy.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class ParkingBoy {
-    public List<ParkingLot> parkingLots;
+    List<ParkingLot> parkingLots;
     public ParkingBoy(ParkingLot parkingLot) {
         this.parkingLots=new ArrayList<>();
         parkingLots.add(parkingLot);
@@ -14,38 +14,29 @@ public class ParkingBoy {
         this.parkingLots=parkingLots;
     }
     public Ticket parking(Car car) throws ParkingException {
-        boolean isFUll=true;
         for(ParkingLot parkingLot:parkingLots){
             if(parkingLot.hasBeenPark(car)){
                 return null;
             }else if(!parkingLot.isFull()){
-                isFUll=false;
                 Ticket ticket=new Ticket();
                 parkingLot.park(ticket,car);
                 return ticket;
             }
         }
-        if(isFUll){
-            throw new ParkingException("Not enough position.");
-        }
-        return null;
+        throw new ParkingException("Not enough position.");
     }
-
     public Car fetchCar(Ticket ticket) throws Exception {
-        boolean isUnrecognized=true;
         if(ticket==null){
             throw new ParkingException("Please provide your parking ticket.");
         }
         for(ParkingLot parkingLot:parkingLots){
             Car car=parkingLot.fetch(ticket);
             if(car!=null){
-                isUnrecognized=false;
                 return car;
             }
         }
-        if(isUnrecognized){
-            throw new ParkingException("Unrecognized parking ticket.");
-        }
-        return null;
+        throw new ParkingException("Unrecognized parking ticket.");
     }
 }
+
+
